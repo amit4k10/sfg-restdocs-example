3cb97ff7557a29924b9d79e1791f69add6924fab
amit4k10 <amit4k10@gmail.com>
2022-03-28 08:47:59 +0530

    master
    master

---
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,70 @@
+package guru.springframework.sfgrestdocsexample.web.mappers;
+
+import guru.springframework.sfgrestdocsexample.domain.Beer;
+import guru.springframework.sfgrestdocsexample.domain.Beer.BeerBuilder;
+import guru.springframework.sfgrestdocsexample.web.model.BeerDto;
+import guru.springframework.sfgrestdocsexample.web.model.BeerDto.BeerDtoBuilder;
+import guru.springframework.sfgrestdocsexample.web.model.BeerStyleEnum;
+import javax.annotation.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-03-28T08:36:07+0530",
+    comments = "version: 1.3.0.Final, compiler: Eclipse JDT (IDE) 3.19.0.v20190903-0936, environment: Java 11.0.5 (Oracle Corporation)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Autowired
+    private DateMapper dateMapper;
+
+    @Override
+    public BeerDto BeerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDtoBuilder beerDto = BeerDto.builder();
+
+        beerDto.beerName( beer.getBeerName() );
+        if ( beer.getBeerStyle() != null ) {
+            beerDto.beerStyle( Enum.valueOf( BeerStyleEnum.class, beer.getBeerStyle() ) );
+        }
+        beerDto.createdDate( dateMapper.asOffsetDateTime( beer.getCreatedDate() ) );
+        beerDto.id( beer.getId() );
+        beerDto.lastModifiedDate( dateMapper.asOffsetDateTime( beer.getLastModifiedDate() ) );
+        beerDto.price( beer.getPrice() );
+        beerDto.upc( beer.getUpc() );
+        if ( beer.getVersion() != null ) {
+            beerDto.version( beer.getVersion().intValue() );
+        }
+
+        return beerDto.build();
+    }
+
+    @Override
+    public Beer BeerDtoToBeer(BeerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        BeerBuilder beer = Beer.builder();
+
+        beer.beerName( dto.getBeerName() );
+        if ( dto.getBeerStyle() != null ) {
+            beer.beerStyle( dto.getBeerStyle().name() );
+        }
+        beer.createdDate( dateMapper.asTimestamp( dto.getCreatedDate() ) );
+        beer.id( dto.getId() );
+        beer.lastModifiedDate( dateMapper.asTimestamp( dto.getLastModifiedDate() ) );
+        beer.price( dto.getPrice() );
+        beer.upc( dto.getUpc() );
+        if ( dto.getVersion() != null ) {
+            beer.version( dto.getVersion().longValue() );
+        }
+
+        return beer.build();
+    }
+}
